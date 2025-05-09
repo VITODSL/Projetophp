965863b0870958c90b8e0cd986f62beef1d34645
VITODSL <vitorlimas714@gmail.com>
2025-05-09 20:17:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+    <html lang="PT-BR">
 
+    <head>
+            <meta charset="UTF-8">
+            <meta name="VIEWPORT" content="widht = device=widht, initial - scale = 10">
+            <title>Dr. Tec</title>
+            <link rel="stylesheck" type="text/css" href="style.css" media="screan"/>
+    </head>
+    <body>
+            <header>
+                    
+
+            </header>
+            <main>
+                    
+
+            </main>
+            <footer>
+                    
+
+            </footer>
+
+    </body>
+
+    </html>
+
+</html>
